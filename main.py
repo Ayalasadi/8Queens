c035c1033d666997b70070f79957ac8fe94dca12
Aya <aya.alasadid@gmail.com>
2025-08-04 00:12:42 -0700

    Add function and minor fixes

--- main.py
+++ main.py
@@ -15,11 +15,31 @@ def print_board(board):
                 line += "Q "
             else:
                 line += ". " 
-        print()
+        print(line)
     print()
+
+def calculate_fitness(board):
+    """
+    Calculates the fitness score of a board.
+    Returns: 28 - number of attacking pairs.
+    """
+    attacking_pairs = 0
+    for i in range(len(board)):
+        for j in range(i + 1, len(board)):
+            #same row
+            if board[i] == board[j]:
+                attacking_pairs += 1
+
+            #same major diagonal
+            elif abs(board[i] - board[j]) == abs(i - j):
+                attacking_pairs += 1
+    return 28 - attacking_pairs
+
 
 
 if __name__ == "__main__":
     board = generate_random_board()
     print("Random board:", board)
     print_board(board)
+    fitness = calculate_fitness(board)
+    print("Fitness score:", fitness)
